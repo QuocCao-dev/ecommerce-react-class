99299319d1e9a635252ec00f377e18378bbe2f9c
Quoc <quoc.cao@spiderbox.design>
2023-10-17 17:22:48 +0700

    add toast

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Navbar from "./components/navbar/Navbar";
+import Navbar from "@/app/components/navbar/Navbar";
+import Notification from "@/app/components/Notification";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Navbar />
         {children}
+        <Notification />
       </body>
     </html>
   );
